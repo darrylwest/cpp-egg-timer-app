2f99fef30b76901748fb517b1dceb89abf8cc28f
darryl west <darryl.west@raincitysoftware.com>
2025-07-03 11:42:50 -0700

    initial implementation

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 #include <spdlog/spdlog.h>
 
+#include <chrono>
+#include <thread>
 #include <app/eggtimer.hpp>
 #include <print>
 #include <string>
@@ -21,6 +23,7 @@ constexpr std::string_view BANNER = R"(
 
 int main(int argc, char** argv) {
     using namespace termio::termio;
+    using namespace std::chrono;
 
     std::println("{}{}\n{}Version: {}{}", green(), BANNER, yellow(), app::eggtimer::VERSION, reset());
 
@@ -30,12 +33,35 @@ int main(int argc, char** argv) {
             spdlog::info("dry run: {} seconds", config.total_seconds);
             return 0;
         }
+
         spdlog::info("Parsed timeout: {} seconds", config.total_seconds);
+        auto tick_time = 1s;
+
+        std::cout << "Timer version: " << VERSION << " started..." << std::endl;
+        auto start = high_resolution_clock::now();
+
+        auto limit = config.total_seconds;
+
+        for (auto i = 0; i < limit; i++) {
+            if (i % 10 == 0) {
+                // std::cout << t << " seconds, " << (limit * 10) - t << " remaining..." << std::endl;
+                std::print("\n{} seconds, {} remaining", i, limit - i);
+            }
+            std::print(".");
+            std::this_thread::sleep_for(tick_time * 1);
+        }
+
+        auto stop = high_resolution_clock::now();
+
+        duration<float> duration = stop - start;
+
+        std::string runner = "afplay /System/Library/Sounds/Funk.aiff";
+        std::println("Timer complete: {} seconds, running: {}", duration.count(), runner);
+
+        return std::system(runner.c_str());
     } catch (const app::eggtimer::CliError& e) {
         spdlog::error("CLI Error: {}", e.what());
         return 1;
     }
-
-    return 0;
 }
 
